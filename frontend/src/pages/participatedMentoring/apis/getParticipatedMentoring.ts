fb34910637b08fd05cdfb6af71b8996258a09e9d
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-11 16:50:22 +0900

    feat: getParticipatedMentoring API 서버 응답을 클라이언트용 데이터로 변환 로직 추가 #370

--- frontend/src/pages/participatedMentoring/apis/getParticipatedMentoring.ts
+++ frontend/src/pages/participatedMentoring/apis/getParticipatedMentoring.ts
@@ -1,11 +1,30 @@
 import { apiClient } from '../../../common/apis/apiClient';
 import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
+import { StatusTypeEnum } from '../../../common/types/statusType';
 
-import type { ParticipatedMentoringType } from '../types/participatedMentoring';
+import type {
+  ClientParticipatedMentoringType,
+  ServerParticipatedMentoringResponse,
+} from '../types/participatedMentoring';
+
+const convertToClientResponse = (
+  serverResponse: ServerParticipatedMentoringResponse[],
+): ClientParticipatedMentoringType[] => {
+  return serverResponse.map((item) => {
+    return {
+      ...item,
+      status: StatusTypeEnum[item.status],
+    };
+  });
+};
 
 export const getParticipatedMentoringList = async () => {
-  return await apiClient.get<ParticipatedMentoringType[]>({
+  const serverResponse = await apiClient.get<
+    ServerParticipatedMentoringResponse[]
+  >({
     endpoint: API_ENDPOINTS.PARTICIPATED_MENTORING,
     withCredentials: true,
   });
+
+  return convertToClientResponse(serverResponse);
 };
