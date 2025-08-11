6d39acc75349f16193e72097201e5f0e5f3f8f3e
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-11 17:23:14 +0900

    refactor: getMentoringApplicationList API 서버 응답을 클라이언트용 데이터로 변환 로직 추가 #370

--- frontend/src/pages/createdMentoring/apis/getMentoringApplicationList.ts
+++ frontend/src/pages/createdMentoring/apis/getMentoringApplicationList.ts
@@ -1,11 +1,30 @@
 import { apiClient } from '../../../common/apis/apiClient';
 import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
+import { StatusTypeEnum } from '../../../common/types/statusType';
 
-import type { MentoringApplicationResponse } from '../types/mentoringApplication';
+import type {
+  ClientMentoringApplication,
+  ServerMentoringApplicationResponse,
+} from '../types/mentoringApplication';
+
+const convertToClientResponse = (
+  response: ServerMentoringApplicationResponse[],
+): ClientMentoringApplication[] => {
+  return response.map((item) => {
+    return {
+      ...item,
+      status: StatusTypeEnum[item.status],
+    };
+  });
+};
 
 export const getMentoringApplicationList = async () => {
-  return await apiClient.get<MentoringApplicationResponse>({
+  const serverResponse = await apiClient.get<
+    ServerMentoringApplicationResponse[]
+  >({
     endpoint: API_ENDPOINTS.CREATED_MENTORING,
     withCredentials: true,
   });
+
+  return convertToClientResponse(serverResponse);
 };
