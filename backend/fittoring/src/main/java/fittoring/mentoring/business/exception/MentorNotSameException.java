3dc43968636cee261268b27bc60a9a81f4961012
kimdev <kim__dev@hufs.ac.kr>
2025-08-12 21:28:15 +0900

    chore: 기능 관련 클래스 추가 #396

---
+++ backend/fittoring/src/main/java/fittoring/mentoring/business/exception/MentorNotSameException.java
@@ -0,0 +1,8 @@
+package fittoring.mentoring.business.exception;
+
+public class MentorNotSameException extends RuntimeException {
+
+    public MentorNotSameException(String message) {
+        super(message);
+    }
+}
