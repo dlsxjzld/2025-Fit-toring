a9713803829af8d752c7672f9ff9b050fccd5328
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-13 22:12:10 +0900

    feat: CertificatesImageModal에 닫기 버튼 추가 및 스타일 수정 #400

--- frontend/src/pages/detail/components/CertificatesImageModal/CertificatesImageModal.tsx
+++ frontend/src/pages/detail/components/CertificatesImageModal/CertificatesImageModal.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 
+import closeImg from '../../../../common/assets/images/white-x-mark.svg';
 import Modal from '../../../../common/components/Modal/Modal';
 
 interface CertificatesImageModalProps {
@@ -17,18 +18,49 @@ function CertificatesImageModal({
 }: CertificatesImageModalProps) {
   return (
     <Modal opened={opened} onCloseClick={onCloseClick}>
-      <StyledContainer src={imageUrl} alt={`${title}`} />
+      <StyledContainer>
+        <StyledCloseButton onClick={onCloseClick}>
+          <StyledCloseImage src={closeImg} alt="모달 닫기 버튼" />
+        </StyledCloseButton>
+        <StyledImage src={imageUrl} alt={`${title}`} />
+      </StyledContainer>
     </Modal>
   );
 }
 
 export default CertificatesImageModal;
 
-const StyledContainer = styled.img`
+const StyledContainer = styled.div`
+  position: relative;
+`;
+
+const StyledImage = styled.img`
   display: flex;
   align-items: center;
   justify-content: center;
 
   width: 100%;
   height: 100%;
+  max-height: 65vh;
+  object-fit: cover;
+`;
+
+const StyledCloseButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: -6rem;
+  right: -2rem;
+
+  padding: 0;
+  border: none;
+
+  background: none;
+  cursor: pointer;
+`;
+
+const StyledCloseImage = styled.img`
+  width: 3rem;
+  height: 3rem;
 `;
