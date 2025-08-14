2eff753f2baba2591c4582e257b065b782514f23
kimyou1102 <yoosk5485@naver.com>
2025-08-14 18:31:39 +0900

    feat: 멘토링 없을 경우 안내문구 추가 및 더미 id 제거 #431
    - 내 멘토링 가져오는 api 사용
    - api 응답값으로 더미데이터 대신 사용

--- frontend/src/pages/createdMentoring/CreatedMentoring.tsx
+++ frontend/src/pages/createdMentoring/CreatedMentoring.tsx
@@ -4,6 +4,7 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 
+import { getMineMentoring } from '../../common/apis/getMineMentoring';
 import Button from '../../common/components/Button/Button';
 import { PAGE_URL } from '../../common/constants/url';
 
@@ -13,6 +14,7 @@ import MentoringApplicationList from './components/MentoringApplicationList/Ment
 
 import type { MentoringApplication } from './types/mentoringApplication';
 import type { StatusType } from '../../common/types/statusType';
+import type { MentoringResponse } from '../detail/types/MentoringResponse';
 
 function CreatedMentoring() {
   const [mentoringApplicationList, setMentoringApplicationList] = useState<
@@ -20,12 +22,6 @@ function CreatedMentoring() {
   >([]);
 
   const navigate = useNavigate();
-
-  const DUMMY_MENTORING_ID = 2;
-
-  const handleMentoringShowButtonClick = () => {
-    navigate(`${PAGE_URL.DETAIL}/${DUMMY_MENTORING_ID}`);
-  };
 
   useEffect(() => {
     const fetchMentoringApplicationList = async () => {
@@ -40,9 +36,28 @@ function CreatedMentoring() {
     fetchMentoringApplicationList();
   }, []);
 
-  if (!mentoringApplicationList.length) {
-    return null;
-  }
+  const [mineMentoring, setMineMentoring] = useState<MentoringResponse | null>(
+    null,
+  );
+
+  const handleMentoringShowButtonClick = () => {
+    if (mineMentoring) {
+      navigate(`${PAGE_URL.DETAIL}/${mineMentoring.id}`);
+    }
+  };
+
+  useEffect(() => {
+    const fetchMentoring = async () => {
+      try {
+        const mentoring = await getMineMentoring();
+        setMineMentoring(mentoring);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMentoring();
+  }, []);
 
   const handleActionButtonsClick = ({
     reservationId,
@@ -69,39 +84,45 @@ function CreatedMentoring() {
 
   return (
     <StyledContainer>
-      <StyledMentoringSectionHeader>
-        <StyledTitle>개설한 멘토링</StyledTitle>
-        <Button
-          onClick={handleMentoringShowButtonClick}
-          customStyle={css`
-            padding: 1rem;
+      {mineMentoring ? (
+        <>
+          <StyledMentoringSectionHeader>
+            <StyledTitle>개설한 멘토링</StyledTitle>
+            <Button
+              onClick={handleMentoringShowButtonClick}
+              customStyle={css`
+                padding: 1rem;
 
-            font-size: 1.4rem;
-          `}
-        >
-          개설한 멘토링 보기
-        </Button>
-      </StyledMentoringSectionHeader>
-      <StyledWrapper>
-        <StyledInfoWrapper>
-          <StyledSubTitle>
-            멘토링 신청 목록 ({mentoringApplicationList.length}건)
-          </StyledSubTitle>
-          <StyledDescription>
-            사용자들이 신청한 멘토링을 승인하거나 거절할 수 있습니다.
-          </StyledDescription>
-        </StyledInfoWrapper>
-        <StyledLine />
-        <MentoringApplicationList>
-          {mentoringApplicationList.map((item) => (
-            <MentoringApplicationItem
-              key={item.reservationId}
-              mentoringApplication={item}
-              onActionButtonsClick={handleActionButtonsClick}
-            />
-          ))}
-        </MentoringApplicationList>
-      </StyledWrapper>
+                font-size: 1.4rem;
+              `}
+            >
+              개설한 멘토링 보기
+            </Button>
+          </StyledMentoringSectionHeader>
+          <StyledWrapper>
+            <StyledInfoWrapper>
+              <StyledSubTitle>
+                멘토링 신청 목록 ({mentoringApplicationList.length}건)
+              </StyledSubTitle>
+              <StyledDescription>
+                사용자들이 신청한 멘토링을 승인하거나 거절할 수 있습니다.
+              </StyledDescription>
+            </StyledInfoWrapper>
+            <StyledLine />
+            <MentoringApplicationList>
+              {mentoringApplicationList.map((item) => (
+                <MentoringApplicationItem
+                  key={item.reservationId}
+                  mentoringApplication={item}
+                  onActionButtonsClick={handleActionButtonsClick}
+                />
+              ))}
+            </MentoringApplicationList>
+          </StyledWrapper>
+        </>
+      ) : (
+        <StyledEmptyText>개설한 멘토링이 없습니다.</StyledEmptyText>
+      )}
     </StyledContainer>
   );
 }
@@ -164,4 +185,8 @@ const StyledLine = styled.hr`
   margin: 0;
   border: none;
   border-top: 1px solid ${({ theme }) => theme.OUTLINE.REGULAR};
+`;
+
+const StyledEmptyText = styled.p`
+  ${({ theme }) => theme.TYPOGRAPHY.B2_R}
 `;
